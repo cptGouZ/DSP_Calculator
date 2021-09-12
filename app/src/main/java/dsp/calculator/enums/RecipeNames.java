03fdd06f9b86105a7ef754a89cb97d531375edb4
CptRed <90554431+CptRed@users.noreply.github.com>
2021-09-12 13:02:37 +0200

    Update RecipeNames.java
    add PM

--- app/src/main/java/dsp/calculator/enums/RecipeNames.java
+++ app/src/main/java/dsp/calculator/enums/RecipeNames.java
@@ -1,8 +1,20 @@
 package dsp.calculator.enums;
 
 public interface RecipeNames {
-    String IRON_INGOT = "iron ingot";
-    String COPPER_INGOT = "copper ingot";
-    String ELECTRICAL_MOTOR = "electrical motor";
-    String GEAR = "gear";
+    String IRON_ORE = "iron ore";
+    String COPPER_ORE = "copper ingot";
+    String STONE = "stone";
+    String COAL = "coal";
+    String SILICON_ORE = "silicon ore";
+    String TITANIUM_ORE = "titanium ore";
+    String WATER = "water";
+    String CRUDE_OIL = "crude oil";
+    String HYDROGEN = "hydrogen";
+    String DEUTERIUM = "deuterium";
+    String ANTIMATTER = "antimatter";
+    String KIMBERLITE_ORE = "kimberlite ore";
+    String FRACTAL_SILICON = "fractal silicon";
+    String OPTICAL_GRATING_CRYSTAL = "optical grating crystal";
+    String SPINIFORM_STALAGMITE_CRYSTAL = "spiniform stalagmite crystal";
+    String UNIPOLAR_MAGNET = "unipolar magnet";
 }
